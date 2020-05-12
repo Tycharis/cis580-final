788c9042c931e1da13dab6705f3410cef4bc2c13
Andy Molina <andy13@ksu.edu>
2020-05-11 23:00:13 -0500

    Worked on upgrades- Not done 90% of art uploaded

--- CIS580-Final/BitcoinMiner.cs
+++ CIS580-Final/BitcoinMiner.cs
@@ -22,6 +22,17 @@ namespace CIS580_Final
         private Texture2D _background;
         private Texture2D _test;
         private Texture2D _bitcoinTexture;
+        private Texture2D _buyButton;
+        private Texture2D _upgradeButton;
+        private Texture2D _textBackground;
+
+        private Texture2D _cpuIcon;
+        private Texture2D _gpuIcon;
+        private Texture2D _minerIcon;
+        private Texture2D _serverIcon;
+        private Texture2D _superComputerIcon;
+
+
 
         //Buttons
         private Button _cpuBuy;
@@ -119,20 +130,21 @@ namespace CIS580_Final
             //Buttons
               //testButton = new Button(724, 300, 300, 100, test);
             _bitcoinButton = new Button(0, 200, 400, 400, _bitcoinTexture);
+            
 
             //Buy buttons
-            _cpuBuy = new Button(954, 275, 60, 30, _test);
-            _gpuBuy = new Button(954, 375, 60, 30, _test);
-            _minerBuy = new Button(954, 475, 60, 30, _test);
-            _serverBuy = new Button(954, 575, 60, 30, _test);
-            _supercomputerBuy = new Button(954, 675, 60, 30, _test);
+            _cpuBuy = new Button(954, 275, 60, 30, _buyButton);
+            _gpuBuy = new Button(954, 375, 60, 30, _buyButton);
+            _minerBuy = new Button(954, 475, 60, 30, _buyButton);
+            _serverBuy = new Button(954, 575, 60, 30, _buyButton);
+            _supercomputerBuy = new Button(954, 675, 60, 30, _buyButton);
 
             //Upgrade buttons
-            _cpuUpgrade = new Button(954, 315, 60, 30, _test);
-            _gpuUpgrade = new Button(954, 415, 60, 30, _test);
-            _minerUpgrade = new Button(954, 515, 60, 30, _test);
-            _serverUpgrade = new Button(954, 615, 60, 30, _test);
-            _supercomputerUpgrade = new Button(954, 715, 60, 30, _test);
+            _cpuUpgrade = new Button(954, 315, 60, 30, _upgradeButton);
+            _gpuUpgrade = new Button(954, 415, 60, 30, _upgradeButton);
+            _minerUpgrade = new Button(954, 515, 60, 30, _upgradeButton);
+            _serverUpgrade = new Button(954, 615, 60, 30, _upgradeButton);
+            _supercomputerUpgrade = new Button(954, 715, 60, 30, _upgradeButton);
 
             //_testButton = new Button(724, 300, 300, 100, _test);
             _bitcoinButton = new Button(0, 200, 400, 400, _bitcoinTexture);
@@ -156,6 +168,19 @@ namespace CIS580_Final
             _background = Content.Load<Texture2D>("GameBG");
             _test = Content.Load<Texture2D>("pixel");
             _bitcoinTexture = Content.Load<Texture2D>("Bitcoin_Coin");
+            _buyButton = Content.Load<Texture2D>("Buy_Button");
+            _upgradeButton = Content.Load<Texture2D>("Upgrade_Button");
+            _textBackground = Content.Load<Texture2D>("Text_Background");
+
+            _cpuIcon = Content.Load<Texture2D>("CPU_Icon");
+            _gpuIcon = Content.Load<Texture2D>("GPU_Icon");
+            //_minerIcon = Content.Load<Texture2D>("Miner_Icon");
+            _serverIcon = Content.Load<Texture2D>("Server_Icon");
+            _superComputerIcon = Content.Load<Texture2D>("SuperComputer_Icon");
+
+
+
+
         }
 
         /// <summary>
@@ -229,31 +254,31 @@ namespace CIS580_Final
                     Console.WriteLine("Bitcoin Button clicked");
                 }
 
-                if (_cpuBuy.IsClicked(_mouseState))
+                if (_cpuBuy.IsClicked(_mouseState) && Bitcoin >= 0.00165   )
                 {
                     buildings.Add(new Building { Type = BuildingType.Cpu });
                     Console.WriteLine("CPU Buy Button clicked");
                 }
 
-                if (_gpuBuy.IsClicked(_mouseState))
+                if (_gpuBuy.IsClicked(_mouseState) && Bitcoin >= 0.011 )
                 {
                     buildings.Add(new Building { Type = BuildingType.Gpu });
                     Console.WriteLine("GPU Buy Button clicked");
                 }
 
-                if (_minerBuy.IsClicked(_mouseState))
+                if (_minerBuy.IsClicked(_mouseState) && Bitcoin >= 0.121 )
                 {
                     buildings.Add(new Building { Type = BuildingType.Miner });
                     Console.WriteLine("Miner Buy Button clicked");
                 }
 
-                if (_serverBuy.IsClicked(_mouseState))
+                if (_serverBuy.IsClicked(_mouseState) && Bitcoin >= 1.32 )
                 {
                     buildings.Add(new Building { Type = BuildingType.Server });
                     Console.WriteLine("Server Buy Button clicked");
                 }
 
-                if (_supercomputerBuy.IsClicked(_mouseState))
+                if (_supercomputerBuy.IsClicked(_mouseState) && Bitcoin >= 14.3)
                 {
                     buildings.Add(new Building { Type = BuildingType.Supercomputer });
                     Console.WriteLine("SuperComputer Buy Button clicked");
@@ -304,22 +329,38 @@ namespace CIS580_Final
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
+
       
             //Display draws
             _spriteBatch.Draw(_background, new Rectangle (0,0,1024,768), Color.White);
-            _spriteBatch.Draw(_test, new Rectangle(50, 150, 300, 40), Color.Wheat);
+            _spriteBatch.Draw(_textBackground, new Rectangle(50, 150, 300, 40), Color.Wheat);
+
+
+
+            //Test Draws
+                _spriteBatch.Draw(_test, new Rectangle(650, 200, 374, 568), Color.DarkRed);
+                // _spriteBatch.Draw(test, new Rectangle(954, 355, 60, 30), Color.DarkRed);
+                // testButton.Draw(_spriteBatch);
+                //_spriteBatch.Draw(_test, new Rectangle(954, 315, 60, 30), Color.DarkRed);
+                //_spriteBatch.Draw(_test, new Rectangle(954, 355, 60, 30), Color.DarkRed);
+
+            //Icon Draws
+            _spriteBatch.Draw(_cpuIcon, new Rectangle(675, 260, 100, 100), Color.Wheat);
+            _spriteBatch.Draw(_gpuIcon, new Rectangle(675, 360, 100, 100), Color.Wheat);
+            //_spriteBatch.Draw(_minerIcon, new Rectangle(675, 460, 100, 100), Color.Wheat);
+            _spriteBatch.Draw(_serverIcon, new Rectangle(675, 560, 100, 100), Color.Wheat);
+            _spriteBatch.Draw(_superComputerIcon, new Rectangle(675, 660, 100, 100), Color.Wheat);
+
+
             //_spriteBatch.DrawString(_font, "Score: " + Bitcoin, new Vector2(50, 150), Color.Black);
 
-            _spriteBatch.DrawString(_font, $"Score: {Bitcoin:0.#####}\nUSD: ${Bitcoin * BtcPerGpu:0.#####}", new Vector2(50, 150), Color.Black);
+            _spriteBatch.DrawString(_font, $"Bitcoins: {Bitcoin:0.#####}\nUSD: ${Bitcoin * BtcPerGpu:0.#####}", new Vector2(60, 153), Color.Black);
 
             //Button draws
           
             _bitcoinButton.Draw(_spriteBatch);
 
-            //Test Draws
-                //_spriteBatch.Draw(test, new Rectangle(954, 315, 60, 30), Color.DarkRed);
-                // _spriteBatch.Draw(test, new Rectangle(954, 355, 60, 30), Color.DarkRed);
-                // testButton.Draw(_spriteBatch);
+           
 
             //Buy button draws
             _cpuBuy.Draw(_spriteBatch);
@@ -335,8 +376,9 @@ namespace CIS580_Final
             _serverUpgrade.Draw(_spriteBatch);
             _supercomputerUpgrade.Draw(_spriteBatch);
 
-            //_spriteBatch.Draw(_test, new Rectangle(954, 315, 60, 30), Color.DarkRed);
-            //_spriteBatch.Draw(_test, new Rectangle(954, 355, 60, 30), Color.DarkRed);
+          
+
+
 
             _spriteBatch.End();
 
